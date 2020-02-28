712554124c2fc8ca872156abf5dfe9f0c15193fd
张炎华 <zhangyh2360@dimpt.com>
2020-02-28 23:34:54 +0800

    complete week 7 Parameters (C, sigma) for Dataset 3

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,6 +23,36 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+v = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+count = size(v,2);
+errorRes = zeros(count*count,3);
+
+smallestError = Inf;
+
+for i=1:count
+    tempC = v(i);
+    for j=1:count
+        i
+        j
+        tempsigma = v(j);
+
+        row = (i-1)*8+j;
+        errorRes(row,1) = tempC;
+        errorRes(row,2) = tempsigma;
+
+        model= svmTrain(X, y, tempC, @(x1, x2) gaussianKernel(x1, x2, tempsigma)); 
+        predictions = svmPredict(model, Xval);
+        modelError = mean(double(predictions ~= yval));
+
+        if modelError < smallestError
+            smallestError = modelError;
+            C = tempC;
+            sigma = tempsigma;
+        end
+
+        errorRes(row,3) = modelError;
+    end
+end
 
 
 
